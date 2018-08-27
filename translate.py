809722a01e16ce2088a8e3f8501e5e45d0e32e7d
katriniya <katriniya.a@gmail.com>
2018-08-27 12:22:05 +0300

    fixed replace and save in json

--- translate.py
+++ translate.py
@@ -1,18 +1,15 @@
 #! /usr/bin/env python
 # coding: utf-8
-from urllib.parse import urlparse
-import json, sys, re, requests, shutil
-
+import json, re, requests, sys
 
 
 URL_TRANSLATE_YANDEX= "https://translate.yandex.net/api/v1.5/tr.json/translate"
-KEY = "YOUR KEY" # enter YOUR key https://translate.yandex.ru/developers/keys
+TOKKEN = "KEY" # enter YOUR key https://translate.yandex.ru/developers/keys
 LANG = "en-ru" 
 
 
 ORIGIN_FILE = 'english.json'
-TRANSLATE_FILE = 'russian.txt'
-
+TRANSLATE_FILE = 'russian.json'
 
 with open(ORIGIN_FILE, 'r') as trans_file:
 	templates = json.load(trans_file)
@@ -24,13 +21,15 @@ for country_name in (templates["data"]):
 
 	normalize_country_name = str(country_name["name"]).replace(' ','+') #replace space of +
 
-	url = URL_TRANSLATE_YANDEX + "?key=" + KEY + "&text=" + normalize_country_name + "&lang=" + LANG
+	url = URL_TRANSLATE_YANDEX + "?key=" + TOKKEN + "&text=" + normalize_country_name + "&lang=" + LANG
+	print (url)
 
 	requests_translate_post = requests.post(url)
 
 	requests_translate_post = requests_translate_post.json()
 
-	translate_file_read=translate_file_read.replace(country_name["name"], str(requests_translate_post["text"])[2:-2])
+	
+	translate_file_read=translate_file_read.replace(country_name["name"], str(requests_translate_post["text"][0]), 1)
 
 	with open(TRANSLATE_FILE, 'w',encoding="utf-8") as file:
 		file.write(translate_file_read)
